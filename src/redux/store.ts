2720ed657e97e4a184953f33d8fb91a74c2f259f
oriolraventosc <oriol.raventos.c@gmail.com>
2022-11-09 15:04:32 +0100

    Create userLogin reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import { robotReducer } from "./features/RobotsSlice/RobotsSlice";
+import { usersReducer } from "./features/UsersSlice/UsersSlice";
 
 export const store = configureStore({
   reducer: {
     robotsActions: robotReducer,
+    usersReducer: usersReducer,
   },
 });
 
